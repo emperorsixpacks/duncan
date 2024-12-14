d0ef43d7d1c3fac5c18ab53f31181f31944c251b
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-14 14:47:44 +0100

    mod: turned of std out to console

--- duncan_test.go
+++ duncan_test.go
@@ -1,10 +1,12 @@
 package duncan
 
 import (
+	"os"
 	"testing"
 )
 
 func TestLoadfromfile(t *testing.T){
+  os.Stdout,  _ = os.Open(os.DevNull)
   err := NewFromConfig("./duncan-config.yml")
   if err != nil{
     t.Error(err)
