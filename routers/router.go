07ab2c1e650b28948084b11cc4a8064bcec04274
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-16 16:53:26 +0100

    update: adding routes:

--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,15 @@
 package routers
 
+import "net/http"
+
 func NewRouter() *Router {
 	return &Router{}
 }
 
 type Router struct {
 	middlewares []string
+	routes      []*Route
 }
 
-func (this *Router) ServeHttp() {}
+func (this *Router) ServeHttp(w http.ResponseWriter, r http.Request) {}
+func (this Route) NewRoute() *Route                                  {}
