0bfaac8ea18981968af9c7099b206459e9992d72
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-16 17:02:44 +0100

    updateL added create route in routers

--- routers/router.go
+++ routers/router.go
@@ -3,13 +3,20 @@ package routers
 import "net/http"
 
 func NewRouter() *Router {
-	return &Router{}
+	return &Router{namedRoutes: make(map[string]*Route)}
 }
 
 type Router struct {
 	middlewares []string
 	routes      []*Route
+	namedRoutes map[string]*Route
 }
 
 func (this *Router) ServeHttp(w http.ResponseWriter, r http.Request) {}
-func (this Route) NewRoute() *Route                                  {}
+
+// This create a new route object
+func (this Router) NewRoute() *Route {
+	route := new(Route)
+	this.routes = append(this.routes, route)
+	return route
+}
