6b7400540aed6d582f0c4d4144775a2711af3610
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-17 16:54:50 +0100

    update: added cache interface

--- types.go
+++ types.go
@@ -7,6 +7,13 @@ type Connection interface {
 	ConnectionString() string
 }
 
-type MiddleWare interface{
-  Hanle(next http.Handler) http.Handler
+type MiddleWare interface {
+	Hanle(next http.Handler) http.Handler
+}
+
+// TODO we have functions taking the same arguments, fix
+type Cache interface {
+	SetJSON(item string, key interface{}, value interface{})
+	GetJSON(item string, key interface{}, o *interface{})
+	DeleteJSON(item string, key interface{}, value interface{})
 }
