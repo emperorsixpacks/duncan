d01f25e64a252f70466bb58fa73b8017beb4b002
emperorsixpacks <andrewdvd10@gmail.com>
2024-12-20 02:39:31 +0100

    feat: working on routers

--- routers/router.go
+++ routers/router.go
@@ -2,21 +2,24 @@ package routers
 
 import "net/http"
 
+// Create a subrouter and add individual middleware and dependencies to that router
+// Router should be able to take a new router object as a subrouter
 func NewRouter() *Router {
 	return &Router{namedRoutes: make(map[string]*Route)}
 }
 
 type Router struct {
-	middlewares []string
-	routes      []*Route
+	middlewares []string // This should be a middlewares interface
+	routes      []*Route // This should be a router interface
 	namedRoutes map[string]*Route
 }
 
 func (this *Router) ServeHttp(w http.ResponseWriter, r http.Request) {}
 
 // This create a new route object
-func (this Router) NewRoute() *Route {
-	route := new(Route)
-	this.routes = append(this.routes, route)
-	return route
+func (this *Router) AddRouter(){}
+func (this *Router) AddRoute(){}
+func (this *Router) GetRoute(name string) *Route{
+  return this.namedRoutes[name]
 }
+
